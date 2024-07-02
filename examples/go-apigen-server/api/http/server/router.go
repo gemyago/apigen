7f5d37fee17489464a474e99f7e87569515fe019
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2024-07-02 12:27:16 +0200

    Adding initial router.

--- examples/go-apigen-server/api/http/server/router.go
+++ examples/go-apigen-server/api/http/server/router.go
@@ -1,10 +1,20 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gemyago/apigen/examples/go-apigen-server/api/http/v1routes/handlers"
+	"github.com/go-chi/chi/v5"
+)
 
 type RoutesDeps struct {
 	PetsController *handlers.PetsController
 }
 
 func NewRouter(deps RoutesDeps) http.Handler {
+	router := chi.NewRouter()
+
+	// TODO: Register routes
+
+	return router
 }
--- examples/go-apigen-server/go.mod
+++ examples/go-apigen-server/go.mod
@@ -1,3 +1,5 @@
 module github.com/gemyago/apigen/examples/go-apigen-server
 
 go 1.22
+
+require github.com/go-chi/chi/v5 v5.1.0
