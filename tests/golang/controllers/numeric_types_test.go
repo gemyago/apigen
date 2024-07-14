4ae5bf1f53029c90f256d01b1096464e3b2055ad
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2024-07-14 16:08:06 +0200

    Reorg test.

--- tests/golang/controllers/numeric_types_test.go
+++ tests/golang/controllers/numeric_types_test.go
@@ -24,6 +24,51 @@ func TestNumericTypes(t *testing.T) {
 		return router
 	}
 
+	type expectFn func(t *testing.T, testActions *numericTypesControllerTestActions, recorder *httptest.ResponseRecorder)
+
+	type testCase struct {
+		path   string
+		query  url.Values
+		expect expectFn
+	}
+
+	// expectErrors := func(wantErrors []handlers.BindingError) expectFn {
+	// 	return func(
+	// 		t *testing.T,
+	// 		testActions *numericTypesControllerTestActions,
+	// 		recorder *httptest.ResponseRecorder,
+	// 	) {
+	// 		assert.Equal(t, 400, recorder.Code)
+	// 		assert.Equal(t, "application/json; charset=utf-8", recorder.Header().Get("content-type"))
+	// 		gotErrors := unmarshalBindingErrors(t, recorder.Body)
+	// 		if !assert.Len(t, gotErrors.Errors, len(wantErrors)) {
+	// 			return
+	// 		}
+	// 		for _, fe := range wantErrors {
+	// 			assertFieldError(t, gotErrors, fe.Location, fe.Field, fe.Code)
+	// 		}
+	// 	}
+	// }
+
+	runTestCase := func(t *testing.T, name string, tc func() testCase) {
+		t.Run(name, func(t *testing.T) {
+			tc := tc()
+			testActions := &numericTypesControllerTestActions{}
+			router := setupRouter(testActions)
+			testReq := httptest.NewRequest(
+				"GET",
+				tc.path,
+				http.NoBody,
+			)
+			if len(tc.query) != 0 {
+				testReq.URL.RawQuery = tc.query.Encode()
+			}
+			recorder := httptest.NewRecorder()
+			router.mux.ServeHTTP(recorder, testReq)
+			tc.expect(t, testActions, recorder)
+		})
+	}
+
 	t.Run("parsing", func(t *testing.T) {
 		randomReq := func() *handlers.NumericTypesNumericTypesParsingRequest {
 			return &handlers.NumericTypesNumericTypesParsingRequest{
@@ -45,15 +90,8 @@ func TestNumericTypes(t *testing.T) {
 			}
 		}
 
-		t.Run("should parse and bind valid values", func(t *testing.T) {
-			testActions := &numericTypesControllerTestActions{}
-			router := setupRouter(testActions)
+		runTestCase(t, "should parse and bind valid values", func() testCase {
 			wantReq := randomReq()
-			testReq := httptest.NewRequest(
-				"GET",
-				fmt.Sprintf("/numeric-types/parsing/%v/%v/%v/%v/%v/%v", wantReq.NumberAny, wantReq.NumberFloat, wantReq.NumberDouble, wantReq.NumberInt, wantReq.NumberInt32, wantReq.NumberInt64),
-				http.NoBody,
-			)
 			query := url.Values{}
 			query.Add("numberAnyInQuery", fmt.Sprint(wantReq.NumberAnyInQuery))
 			query.Add("numberFloatInQuery", fmt.Sprint(wantReq.NumberFloatInQuery))
@@ -61,13 +99,15 @@ func TestNumericTypes(t *testing.T) {
 			query.Add("numberIntInQuery", fmt.Sprint(wantReq.NumberIntInQuery))
 			query.Add("numberInt32InQuery", fmt.Sprint(wantReq.NumberInt32InQuery))
 			query.Add("numberInt64InQuery", fmt.Sprint(wantReq.NumberInt64InQuery))
-			testReq.URL.RawQuery = query.Encode()
-			recorder := httptest.NewRecorder()
-			router.mux.ServeHTTP(recorder, testReq)
 
-			assert.Equal(t, 204, recorder.Code)
-			assert.Len(t, testActions.numericTypesParsing, 1)
-			assert.Equal(t, wantReq, testActions.numericTypesParsing[0].params)
+			return testCase{
+				path:  fmt.Sprintf("/numeric-types/parsing/%v/%v/%v/%v/%v/%v", wantReq.NumberAny, wantReq.NumberFloat, wantReq.NumberDouble, wantReq.NumberInt, wantReq.NumberInt32, wantReq.NumberInt64),
+				query: query,
+				expect: func(t *testing.T, testActions *numericTypesControllerTestActions, recorder *httptest.ResponseRecorder) {
+					assert.Equal(t, 204, recorder.Code)
+					assert.Equal(t, wantReq, testActions.numericTypesParsing[0].params)
+				},
+			}
 		})
 	})
 }
