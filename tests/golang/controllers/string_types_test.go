3adb1941ee6f4b8f98d6401894febe297c32e53e
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2024-07-16 18:04:38 +0200

    Required validation for string.

--- tests/golang/controllers/string_types_test.go
+++ tests/golang/controllers/string_types_test.go
@@ -264,4 +264,131 @@ func TestStringTypes(t *testing.T) {
 			}
 		})
 	})
+
+	t.Run("required-validation", func(t *testing.T) {
+		randomReq := func(
+			opts ...func(*handlers.StringTypesStringTypesRequiredValidationRequest),
+		) *handlers.StringTypesStringTypesRequiredValidationRequest {
+			req := &handlers.StringTypesStringTypesRequiredValidationRequest{
+				// query
+				UnformattedStrInQuery:  fake.BinaryString().BinaryString(fake.IntBetween(10, 20)),
+				CustomFormatStrInQuery: fake.BinaryString().BinaryString(fake.IntBetween(20, 30)),
+				DateStrInQuery:         fake.Time().Time(time.Now()),
+				DateTimeStrInQuery:     fake.Time().Time(time.Now()),
+				ByteStrInQuery:         fake.BinaryString().BinaryString(fake.IntBetween(30, 40)),
+
+				OptionalUnformattedStrInQuery:  fake.BinaryString().BinaryString(fake.IntBetween(10, 20)),
+				OptionalCustomFormatStrInQuery: fake.BinaryString().BinaryString(fake.IntBetween(20, 30)),
+				OptionalDateStrInQuery:         fake.Time().Time(time.Now()),
+				OptionalDateTimeStrInQuery:     fake.Time().Time(time.Now()),
+				OptionalByteStrInQuery:         fake.BinaryString().BinaryString(fake.IntBetween(30, 40)),
+			}
+			for _, opt := range opts {
+				opt(req)
+			}
+			return req
+		}
+
+		buildQuery := func(wantReq *handlers.StringTypesStringTypesRequiredValidationRequest) url.Values {
+			query := url.Values{}
+			query.Add("unformattedStrInQuery", wantReq.UnformattedStrInQuery)
+			query.Add("customFormatStrInQuery", wantReq.CustomFormatStrInQuery)
+			query.Add("dateStrInQuery", wantReq.DateStrInQuery.Format(time.RFC3339Nano))
+			query.Add("dateTimeStrInQuery", wantReq.DateTimeStrInQuery.Format(time.RFC3339Nano))
+			query.Add("byteStrInQuery", wantReq.ByteStrInQuery)
+
+			query.Add("optionalUnformattedStrInQuery", wantReq.OptionalUnformattedStrInQuery)
+			query.Add("optionalCustomFormatStrInQuery", wantReq.OptionalCustomFormatStrInQuery)
+			query.Add("optionalDateStrInQuery", wantReq.OptionalDateStrInQuery.Format(time.RFC3339Nano))
+			query.Add("optionalDateTimeStrInQuery", wantReq.OptionalDateTimeStrInQuery.Format(time.RFC3339Nano))
+			query.Add("optionalByteStrInQuery", wantReq.OptionalByteStrInQuery)
+			return query
+		}
+
+		runRouteTestCase(t, "should parse optional params", setupRouter, func() testCase {
+			originalReq := randomReq()
+			query := buildQuery(originalReq)
+			return testCase{
+				path:  "/string-types/required-validation",
+				query: query,
+				expect: func(t *testing.T, testActions *stringTypesControllerTestActions, recorder *httptest.ResponseRecorder) {
+					if !assert.Equal(t, 204, recorder.Code) {
+						t.Fatalf("unexpected response: %v", recorder.Body)
+					}
+
+					wantReq := *originalReq
+					wantReq.DateStrInQuery = originalReq.DateStrInQuery.Truncate(24 * time.Hour)
+					wantReq.OptionalDateStrInQuery = originalReq.OptionalDateStrInQuery.Truncate(24 * time.Hour)
+					assert.Equal(t, &wantReq, testActions.StringTypesRequiredValidation.calls[0].params)
+				},
+			}
+		})
+
+		runRouteTestCase(t, "should ignore missing optional params", setupRouter, func() testCase {
+			originalReq := randomReq(func(req *handlers.StringTypesStringTypesRequiredValidationRequest) {
+				req.OptionalUnformattedStrInQuery = ""
+				req.OptionalCustomFormatStrInQuery = ""
+				req.OptionalDateStrInQuery = time.Time{}
+				req.OptionalDateTimeStrInQuery = time.Time{}
+				req.OptionalByteStrInQuery = ""
+			})
+
+			query := buildQuery(originalReq)
+			query.Del("optionalUnformattedStrInQuery")
+			query.Del("optionalCustomFormatStrInQuery")
+			query.Del("optionalDateStrInQuery")
+			query.Del("optionalDateTimeStrInQuery")
+			query.Del("optionalByteStrInQuery")
+
+			return testCase{
+				path:  "/string-types/required-validation",
+				query: query,
+				expect: func(t *testing.T, testActions *stringTypesControllerTestActions, recorder *httptest.ResponseRecorder) {
+					if !assert.Equal(t, 204, recorder.Code) {
+						t.Fatalf("unexpected response: %v", recorder.Body)
+					}
+
+					wantReq := *originalReq
+					wantReq.DateStrInQuery = originalReq.DateStrInQuery.Truncate(24 * time.Hour)
+					assert.Equal(t, &wantReq, testActions.StringTypesRequiredValidation.calls[0].params)
+				},
+			}
+		})
+
+		runRouteTestCase(t, "should validate empty params", setupRouter, func() testCase {
+			query := url.Values{}
+
+			query.Set("unformattedStrInQuery", "")
+			query.Set("customFormatStrInQuery", "")
+			query.Set("dateStrInQuery", "")
+			query.Set("dateTimeStrInQuery", "")
+			query.Set("byteStrInQuery", "")
+
+			query.Set("optionalUnformattedStrInQuery", "")
+			query.Set("optionalCustomFormatStrInQuery", "")
+			query.Set("optionalDateStrInQuery", "")
+			query.Set("optionalDateTimeStrInQuery", "")
+			query.Set("optionalByteStrInQuery", "")
+
+			return testCase{
+				path:  "/string-types/required-validation",
+				query: query,
+				expect: expectBindingErrors[*stringTypesControllerTestActions](
+					[]handlers.BindingError{
+						{Field: "unformattedStrInQuery", Location: "query", Code: handlers.ErrInvalidValueOutOfRange},
+						{Field: "customFormatStrInQuery", Location: "query", Code: handlers.ErrInvalidValueOutOfRange},
+						{Field: "dateStrInQuery", Location: "query", Code: handlers.ErrBadValueFormat},
+						{Field: "dateTimeStrInQuery", Location: "query", Code: handlers.ErrBadValueFormat},
+						{Field: "byteStrInQuery", Location: "query", Code: handlers.ErrInvalidValueOutOfRange},
+
+						{Field: "optionalUnformattedStrInQuery", Location: "query", Code: handlers.ErrInvalidValueOutOfRange},
+						{Field: "optionalCustomFormatStrInQuery", Location: "query", Code: handlers.ErrInvalidValueOutOfRange},
+						{Field: "optionalDateStrInQuery", Location: "query", Code: handlers.ErrBadValueFormat},
+						{Field: "optionalDateTimeStrInQuery", Location: "query", Code: handlers.ErrBadValueFormat},
+						{Field: "optionalByteStrInQuery", Location: "query", Code: handlers.ErrInvalidValueOutOfRange},
+					},
+				),
+			}
+		})
+	})
 }
