7539112fcbcb78ed7aa397d22b25dca5ad5a42a4
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2024-07-16 09:36:03 +0200

    Parsing dates.

--- tests/golang/controllers/string_types_test.go
+++ tests/golang/controllers/string_types_test.go
@@ -34,34 +34,63 @@ func TestStringTypes(t *testing.T) {
 				// path
 				UnformattedStr:  fake.Lorem().Word(),
 				CustomFormatStr: fake.Lorem().Word(),
-				DateStr:         fake.Time().Faker.Time().ISO8601(time.Now()),
-				DateTimeStr:     fake.Time().Faker.Time().ISO8601(time.Now()),
+				DateStr:         fake.Time().Time(time.Now()),
+				DateTimeStr:     fake.Time().Time(time.Now()),
 				ByteStr:         fake.BinaryString().BinaryString(10),
 
 				// query
 				UnformattedStrInQuery:  fake.Lorem().Word(),
 				CustomFormatStrInQuery: fake.Lorem().Word(),
-				DateStrInQuery:         fake.Time().Faker.Time().ISO8601(time.Now()),
-				DateTimeStrInQuery:     fake.Time().Faker.Time().ISO8601(time.Now()),
+				DateStrInQuery:         fake.Time().Time(time.Now()),
+				DateTimeStrInQuery:     fake.Time().Time(time.Now()),
 				ByteStrInQuery:         fake.BinaryString().BinaryString(10),
 			}
 		}
 		runRouteTestCase(t, "should parse and bind valid values", setupRouter, func() testCase {
-			wantReq := randomReq()
+			originalReq := randomReq()
 			query := url.Values{}
-			query.Add("unformattedStrInQuery", wantReq.UnformattedStrInQuery)
-			query.Add("customFormatStrInQuery", wantReq.CustomFormatStrInQuery)
-			query.Add("dateStrInQuery", wantReq.DateStrInQuery)
-			query.Add("dateTimeStrInQuery", wantReq.DateTimeStrInQuery)
-			query.Add("byteStrInQuery", wantReq.ByteStrInQuery)
+			query.Add("unformattedStrInQuery", originalReq.UnformattedStrInQuery)
+			query.Add("customFormatStrInQuery", originalReq.CustomFormatStrInQuery)
+			query.Add("dateStrInQuery", originalReq.DateStrInQuery.Format(time.RFC3339Nano))
+			query.Add("dateTimeStrInQuery", originalReq.DateTimeStrInQuery.Format(time.RFC3339Nano))
+			query.Add("byteStrInQuery", originalReq.ByteStrInQuery)
 
 			return testCase{
-				path:  fmt.Sprintf("/string-types/parsing/%v/%v/%v/%v/%v", wantReq.UnformattedStr, wantReq.CustomFormatStr, wantReq.DateStr, wantReq.DateTimeStr, wantReq.ByteStr),
+				path:  fmt.Sprintf("/string-types/parsing/%v/%v/%v/%v/%v", originalReq.UnformattedStr, originalReq.CustomFormatStr, originalReq.DateStr.Format(time.RFC3339Nano), originalReq.DateTimeStr.Format(time.RFC3339Nano), originalReq.ByteStr),
 				query: query,
 				expect: func(t *testing.T, testActions *stringTypesControllerTestActions, recorder *httptest.ResponseRecorder) {
 					assert.Equal(t, 204, recorder.Code)
-					assert.Equal(t, wantReq, testActions.stringTypesParsing.calls[0].params)
+
+					wantReq := *originalReq
+					wantReq.DateStr = originalReq.DateStr.Truncate(24 * time.Hour)
+					wantReq.DateStrInQuery = originalReq.DateStrInQuery.Truncate(24 * time.Hour)
+					assert.Equal(t, &wantReq, testActions.stringTypesParsing.calls[0].params)
 				},
+			}
+		})
+		runRouteTestCase(t, "should fail if bad values", setupRouter, func() testCase {
+			originalReq := randomReq()
+			query := url.Values{}
+			query.Add("unformattedStrInQuery", originalReq.UnformattedStrInQuery)
+			query.Add("customFormatStrInQuery", originalReq.CustomFormatStrInQuery)
+			query.Add("dateStrInQuery", fake.Lorem().Word())
+			query.Add("dateTimeStrInQuery", fake.Lorem().Word())
+			query.Add("byteStrInQuery", originalReq.ByteStrInQuery)
+
+			return testCase{
+				path:  fmt.Sprintf("/string-types/parsing/%v/%v/%v/%v/%v", originalReq.UnformattedStr, originalReq.CustomFormatStr, fake.Lorem().Word(), fake.Lorem().Word(), originalReq.ByteStr),
+				query: query,
+				expect: expectBindingErrors[*stringTypesControllerTestActions](
+					[]handlers.BindingError{
+						// path
+						{Field: "dateStr", Location: "path", Code: handlers.ErrBadValueFormat},
+						{Field: "dateTimeStr", Location: "path", Code: handlers.ErrBadValueFormat},
+
+						// query
+						{Field: "dateStrInQuery", Location: "query", Code: handlers.ErrBadValueFormat},
+						{Field: "dateTimeStrInQuery", Location: "query", Code: handlers.ErrBadValueFormat},
+					},
+				),
 			}
 		})
 	})
