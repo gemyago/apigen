b7969e1938077f670d62ca54a733b029d0ef836f
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2024-08-19 11:27:26 +0200

    Initial boolean test.

--- tests/golang/controllers/boolean.go
+++ tests/golang/controllers/boolean.go
@@ -8,6 +8,7 @@ type booleanControllerTestActions struct {
 	booleanParsing            mockAction[*handlers.BooleanBooleanParsingRequest]
 	booleanRequiredValidation mockAction[*handlers.BooleanBooleanRequiredValidationRequest]
 	booleanNullable           mockAction[*handlers.BooleanBooleanNullableRequest]
+	booleanArrayItems         mockAction[*handlers.BooleanBooleanArrayItemsRequest]
 }
 
 func newBooleanController(
@@ -17,5 +18,6 @@ func newBooleanController(
 		HandleBooleanParsing.With(testActions.booleanParsing.action).
 		HandleBooleanRequiredValidation.With(testActions.booleanRequiredValidation.action).
 		HandleBooleanNullable.With(testActions.booleanNullable.action).
+		HandleBooleanArrayItems.With(testActions.booleanArrayItems.action).
 		Finalize()
 }
