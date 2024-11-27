9e723762b27530347f3da951e79df1e89845d6d0
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2024-11-27 21:45:21 +0100

    Support file sources.

--- lang/go/apigen/internal/cmd/generator.go
+++ lang/go/apigen/internal/cmd/generator.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	"golang.org/x/net/context/ctxhttp"
 )
@@ -26,18 +27,31 @@ type ResourceDownloader func(ctx context.Context, src, dst string) error
 
 func NewResourceDownloader() ResourceDownloader {
 	return func(ctx context.Context, src, dst string) error {
-		res, err := ctxhttp.Get(ctx, http.DefaultClient, src)
-		if err != nil {
-			return fmt.Errorf("failed to download %s: %w", src, err)
+		var srcStream io.ReadCloser
+		if strings.Index(src, "http") == 0 {
+			res, err := ctxhttp.Get(ctx, http.DefaultClient, src)
+			if err != nil {
+				return fmt.Errorf("failed to download %s: %w", src, err)
+			}
+			defer res.Body.Close()
+			srcStream = res.Body
+		} else {
+			if strings.Index(src, "file") == 0 {
+				src = src[7:] // remove "file://"
+			}
+			file, err := os.Open(src)
+			if err != nil {
+				return fmt.Errorf("failed to open source file %s: %w", src, err)
+			}
+			defer file.Close()
+			srcStream = file
 		}
-		defer res.Body.Close()
-		// need to copy res body to dst file path
 		dstFile, err := os.Create(dst)
 		if err != nil {
 			return fmt.Errorf("failed to create file %s: %w", dst, err)
 		}
 		defer dstFile.Close()
-		if _, err = io.Copy(dstFile, res.Body); err != nil {
+		if _, err = io.Copy(dstFile, srcStream); err != nil {
 			return fmt.Errorf("failed to write to file %s: %w", dst, err)
 		}
 		return nil
--- lang/go/apigen/internal/cmd/generator_test.go
+++ lang/go/apigen/internal/cmd/generator_test.go
@@ -15,7 +15,7 @@ import (
 )
 
 func TestResourceDownloader(t *testing.T) {
-	t.Run("should download the resource to a given location", func(t *testing.T) {
+	t.Run("should download the resource from a given url", func(t *testing.T) {
 		outputDir := t.TempDir()
 		outputFile := path.Join(outputDir, faker.Word()+".txt")
 
@@ -28,6 +28,42 @@ func TestResourceDownloader(t *testing.T) {
 		downloader := NewResourceDownloader()
 
 		err := downloader(context.Background(), srv.URL, outputFile)
+		require.NoError(t, err)
+
+		content, err := os.ReadFile(outputFile)
+		require.NoError(t, err)
+		assert.Equal(t, sourceContent, string(content))
+	})
+
+	t.Run("should copy the resource if it's not an url", func(t *testing.T) {
+		outputDir := t.TempDir()
+		outputFile := path.Join(outputDir, faker.Word()+".txt")
+
+		sourceContent := faker.Sentence()
+		sourceFile := path.Join(outputDir, faker.Word()+".txt")
+		require.NoError(t, os.WriteFile(sourceFile, []byte(sourceContent), 0644))
+
+		downloader := NewResourceDownloader()
+
+		err := downloader(context.Background(), sourceFile, outputFile)
+		require.NoError(t, err)
+
+		content, err := os.ReadFile(outputFile)
+		require.NoError(t, err)
+		assert.Equal(t, sourceContent, string(content))
+	})
+
+	t.Run("should copy the resource if it's a file url", func(t *testing.T) {
+		outputDir := t.TempDir()
+		outputFile := path.Join(outputDir, faker.Word()+".txt")
+
+		sourceContent := faker.Sentence()
+		sourceFile := path.Join(outputDir, faker.Word()+".txt")
+		require.NoError(t, os.WriteFile(sourceFile, []byte(sourceContent), 0644))
+
+		downloader := NewResourceDownloader()
+
+		err := downloader(context.Background(), "file://"+sourceFile, outputFile)
 		require.NoError(t, err)
 
 		content, err := os.ReadFile(outputFile)
