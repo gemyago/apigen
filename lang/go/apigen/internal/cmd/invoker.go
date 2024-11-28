285755305a336e804f26a2809b844c9c04f6ffd6
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2024-11-28 23:30:49 +0100

    Adding initial invoker.

---
+++ lang/go/apigen/internal/cmd/invoker.go
@@ -0,0 +1,96 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os/exec"
+	"sync"
+)
+
+type GeneratorInvokerParams struct {
+	// Input is a path to the Input OpenAPI specification file
+	Input string
+
+	// Output is a path to the Output directory where the generated code will be placed
+	Output string
+
+	// OagCliLocation is a path to the source openapi-generator-cli jar file
+	OagCliLocation string
+
+	// GeneratorLocation is a path to the source generator plugin jar file
+	GeneratorLocation string
+}
+
+type osExecutableCmd interface {
+	Run() error
+	StderrPipe() (io.ReadCloser, error)
+	StdoutPipe() (io.ReadCloser, error)
+}
+
+var _ osExecutableCmd = (*exec.Cmd)(nil)
+
+type GeneratorInvokerDeps struct {
+	stdOut                     io.Writer
+	stdErr                     io.Writer
+	osExecutableCmdFactoryFunc func(name string, arg ...string) osExecutableCmd
+}
+
+type GeneratorInvoker func(ctx context.Context, params GeneratorInvokerParams) error
+
+func NewGeneratorInvoker(
+	deps GeneratorInvokerDeps,
+) GeneratorInvoker {
+	return func(_ context.Context, params GeneratorInvokerParams) error {
+		cmd := deps.osExecutableCmdFactoryFunc(
+			"java",
+			"-cp",
+			params.OagCliLocation+":"+params.GeneratorLocation,
+			"org.openapitools.codegen.OpenAPIGenerator",
+			"generate",
+			"-g",
+			"go-apigen-server",
+			"-i",
+			params.Input,
+			"-o",
+			params.Output,
+		)
+
+		stdOut, err := cmd.StdoutPipe()
+		if err != nil {
+			return fmt.Errorf("failed to get stdout pipe: %w", err)
+		}
+		defer stdOut.Close()
+
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			if _, pipeErr := io.Copy(deps.stdOut, stdOut); pipeErr != nil {
+				fmt.Fprintf(deps.stdErr, "failed to copy stdout: %v", pipeErr)
+			}
+			wg.Done()
+		}()
+
+		stdErr, err := cmd.StderrPipe()
+		if err != nil {
+			return fmt.Errorf("failed to get stderr pipe: %w", err)
+		}
+		defer stdErr.Close()
+
+		wg.Add(1)
+		go func() {
+			if _, pipeErr := io.Copy(deps.stdErr, stdErr); pipeErr != nil {
+				fmt.Fprintf(deps.stdErr, "failed to copy stderr: %v", pipeErr)
+			}
+			wg.Done()
+		}()
+
+		if err = cmd.Run(); err != nil {
+			return err
+		}
+
+		wg.Wait()
+
+		return nil
+	}
+}
---
+++ lang/go/apigen/internal/cmd/invoker_test.go
@@ -0,0 +1,90 @@
+package cmd
+
+import (
+	"context"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/go-faker/faker/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type mockOsExecutableCmd struct {
+	runCallsCount int
+	stdOut        string
+	stdErr        string
+}
+
+func (m *mockOsExecutableCmd) Run() error {
+	m.runCallsCount++
+	return nil
+}
+
+func (m *mockOsExecutableCmd) StderrPipe() (io.ReadCloser, error) {
+	return io.NopCloser(strings.NewReader(m.stdErr)), nil
+}
+
+func (m *mockOsExecutableCmd) StdoutPipe() (io.ReadCloser, error) {
+	return io.NopCloser(strings.NewReader(m.stdOut)), nil
+}
+
+func TestGeneratorInvoker(t *testing.T) {
+	t.Run("should invoke the generator and pipe stdout and stderr", func(t *testing.T) {
+		/*
+			java -cp $(cli_jar):$(golang_server_jar) \
+				org.openapitools.codegen.OpenAPIGenerator generate \
+				-g go-apigen-server \
+				-i $< \
+				-o $@
+		*/
+		wantStdOut := faker.Sentence()
+		wantStdErr := faker.Sentence()
+
+		oagCliLocation := faker.URL()
+		generatorLocation := faker.URL()
+		inputLocation := faker.URL()
+		outputLocation := faker.URL()
+
+		mockStdOut := &strings.Builder{}
+		mockStdErr := &strings.Builder{}
+		wantCmd := &mockOsExecutableCmd{
+			stdOut: wantStdOut,
+			stdErr: wantStdErr,
+		}
+
+		invoker := NewGeneratorInvoker(GeneratorInvokerDeps{
+			stdOut: mockStdOut,
+			stdErr: mockStdErr,
+			osExecutableCmdFactoryFunc: func(name string, arg ...string) osExecutableCmd {
+				assert.Equal(t, "java", name)
+				assert.Equal(t, []string{
+					"-cp",
+					oagCliLocation + ":" + generatorLocation,
+					"org.openapitools.codegen.OpenAPIGenerator",
+					"generate",
+					"-g",
+					"go-apigen-server",
+					"-i",
+					inputLocation,
+					"-o",
+					outputLocation,
+				}, arg)
+				return wantCmd
+			},
+		})
+
+		err := invoker(context.Background(), GeneratorInvokerParams{
+			Input:             inputLocation,
+			Output:            outputLocation,
+			OagCliLocation:    oagCliLocation,
+			GeneratorLocation: generatorLocation,
+		})
+		require.NoError(t, err)
+
+		assert.Equal(t, wantStdOut, mockStdOut.String())
+		assert.Equal(t, wantStdErr, mockStdErr.String())
+		assert.Equal(t, 1, wantCmd.runCallsCount)
+	})
+}
