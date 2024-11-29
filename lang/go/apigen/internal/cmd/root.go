ab440e18721bb0af5688f5bc966a610501071e46
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2024-11-29 23:48:31 +0100

    Rework init.

--- lang/go/apigen/internal/cmd/root.go
+++ lang/go/apigen/internal/cmd/root.go
@@ -1,18 +1,52 @@
 package cmd
 
 import (
+	"io/fs"
+	"log/slog"
+	"os"
+	"os/exec"
+
+	"github.com/gemyago/apigen/lang/go/apigen/internal/cmd/resources"
 	"github.com/spf13/cobra"
 )
 
 const expectedArgsCount = 2
 
-func NewRootCmd(generator Generator) *cobra.Command {
+func NewRootCmd() *cobra.Command {
 	var params GeneratorParams
+	verbose := false
+
+	var generator Generator
 
 	cmd := &cobra.Command{
 		Use:   "apigengo [input] [output]",
 		Short: "Generate HTTP layer from OpenAPI spec",
 		Args:  cobra.ExactArgs(expectedArgsCount),
+		PreRun: func(_ *cobra.Command, _ []string) {
+			level := slog.LevelWarn
+			if verbose {
+				level = slog.LevelDebug
+			}
+			rootLogger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+				Level: level,
+			}))
+
+			generator = NewGenerator(GeneratorDeps{
+				RootLogger: rootLogger,
+				SupportFilesInstaller: NewSupportFilesInstaller(SupportFilesInstallerDeps{
+					Downloader: NewResourceDownloader(),
+					RootFS:     os.DirFS("/").(fs.ReadFileFS), //TODO: without the cast?
+				}),
+				MetadataReader: resources.NewMetadataReader(),
+				GeneratorInvoker: NewGeneratorInvoker(GeneratorInvokerDeps{
+					StdOut: os.Stdout,
+					StdErr: os.Stderr,
+					OsExecutableCmdFactoryFunc: func(name string, arg ...string) OsExecutableCmd {
+						return exec.Command(name, arg...)
+					},
+				}),
+			})
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			params.input = args[0]
 			params.output = args[1]
@@ -33,5 +67,6 @@ func NewRootCmd(generator Generator) *cobra.Command {
 	pFlags.StringVar(&params.generatorLocation, "generator-location", "",
 		"Alternative location of the generator plugin jar file "+
 			"(can be file or url)")
+	pFlags.BoolVar(&verbose, "verbose", verbose, "Enable verbose output")
 	return cmd
 }
--- lang/go/apigen/main.go
+++ lang/go/apigen/main.go
@@ -1,30 +1,13 @@
 package main
 
 import (
-	"io/fs"
 	"os"
-	"os/exec"
 
 	"github.com/gemyago/apigen/lang/go/apigen/internal/cmd"
-	"github.com/gemyago/apigen/lang/go/apigen/internal/cmd/resources"
 )
 
 func main() {
-	generator := cmd.NewGenerator(cmd.GeneratorDeps{
-		SupportFilesInstaller: cmd.NewSupportFilesInstaller(cmd.SupportFilesInstallerDeps{
-			Downloader: cmd.NewResourceDownloader(),
-			RootFS:     os.DirFS("/").(fs.ReadFileFS), //TODO: without the cast?
-		}),
-		MetadataReader: resources.NewMetadataReader(),
-		GeneratorInvoker: cmd.NewGeneratorInvoker(cmd.GeneratorInvokerDeps{
-			StdOut: os.Stdout,
-			StdErr: os.Stderr,
-			OsExecutableCmdFactoryFunc: func(name string, arg ...string) cmd.OsExecutableCmd {
-				return exec.Command(name, arg...)
-			},
-		}),
-	})
-	rootCmd := cmd.NewRootCmd(generator)
+	rootCmd := cmd.NewRootCmd()
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
