831a13f185a7ef8ee22c9aad974c33d68d3a31f0
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2024-11-30 19:13:34 +0100

    Adding explicit version.

--- lang/go/apigen/internal/cmd/resources/resources.go
+++ lang/go/apigen/internal/cmd/resources/resources.go
@@ -11,6 +11,7 @@ import (
 
 //go:generate cp -r ../../../../../../generators/go-apigen-server/pom.xml ./go-apigen-server.xml
 //go:generate cp -r ../../../../../../.versions ./.versions
+//go:generate cp -r ../../../../../../version ./version
 
 //go:embed go-apigen-server.xml .versions
 var resources embed.FS
