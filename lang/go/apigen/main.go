79d98f04f791a7f4d938241131fb1845281bafcd
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2024-11-30 10:54:53 +0100

    Temp stuff.

--- lang/go/apigen/main.go
+++ lang/go/apigen/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/gemyago/apigen/lang/go/apigen/internal/cmd"
 )
 
 func main() {
+	bi, ok := debug.ReadBuildInfo()
+	fmt.Println("bi", bi, "ok", ok)
+
 	rootCmd := cmd.NewRootCmd()
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
