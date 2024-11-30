c59ce596e60dc0747568ae1dd1839aea999e64e3
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2024-11-30 19:59:33 +0100

    Tidy.

--- lang/go/apigen/main.go
+++ lang/go/apigen/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"runtime/debug"
 
 	"github.com/gemyago/apigen/lang/go/apigen/internal/cmd"
 )
 
 func main() {
-	bi, ok := debug.ReadBuildInfo()
-	fmt.Println("bi", bi, "ok", ok)
-
 	rootCmd := cmd.NewRootCmd()
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
