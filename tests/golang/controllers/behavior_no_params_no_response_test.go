0f55d6744e9a646f247c5afed2441058e9a04235
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-02-02 23:29:42 +0100

    Some tests.

---
+++ tests/golang/controllers/behavior_no_params_no_response_test.go
@@ -0,0 +1,61 @@
+package controllers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gemyago/apigen/tests/golang/routes/handlers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBehaviorNoParamsNoResponse(t *testing.T) {
+	setupRouter := func() (*behaviorControllerNoParamsNoResponseTestActions, http.Handler) {
+		testActions := &behaviorControllerNoParamsNoResponseTestActions{}
+		controller := newBehaviorControllerNoParamsNoResponse(testActions)
+		router := &routerAdapter{
+			mux: http.NewServeMux(),
+		}
+		handlers.RegisterBehaviorNoParamsNoResponseIsolatedRoutes(
+			controller,
+			handlers.NewHTTPApp(router, handlers.WithLogger(newLogger())),
+		)
+		return testActions, router.mux
+	}
+
+	t.Run("controller builder", func(t *testing.T) {
+		t.Run("should panic if actions are not initialized", func(t *testing.T) {
+			assert.PanicsWithError(t, "behaviorNoParamsNoResponse action has not been initialized", func() {
+				handlers.BuildBehaviorNoParamsNoResponseIsolatedController().Finalize()
+			})
+		})
+
+		t.Run("should build the controller if all actions are initialized", func(t *testing.T) {
+			assert.NotPanics(t, func() {
+				newBehaviorControllerNoParamsNoResponse(&behaviorControllerNoParamsNoResponseTestActions{})
+			})
+		})
+	})
+
+	type testCase = routeTestCase[*behaviorControllerNoParamsNoResponseTestActions]
+
+	t.Run("noParamsNoResponse", func(t *testing.T) {
+		runRouteTestCase(t, "should process the request", setupRouter, func() testCase {
+			return testCase{
+				method: http.MethodGet,
+				path:   "/behavior/no-params-no-response-isolated",
+				expect: func(
+					t *testing.T,
+					testActions *behaviorControllerNoParamsNoResponseTestActions,
+					recorder *httptest.ResponseRecorder,
+				) {
+					if !assert.Equal(t, 202, recorder.Code, "Unexpected response: %v", recorder.Body) {
+						return
+					}
+
+					assert.Len(t, testActions.noParamsNoResponse.calls, 1)
+				},
+			}
+		})
+	})
+}
