1bb807ff03d7f56b091f79d5f92e178c48826528
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-02-02 17:59:03 +0100

    Fix unused imports

--- tests/golang/routes/handlers/behavior_no_params_no_response_isolated_params.go
+++ tests/golang/routes/handlers/behavior_no_params_no_response_isolated_params.go
@@ -6,7 +6,9 @@ import (
 	. "github.com/gemyago/apigen/tests/golang/routes/internal"
 )
 
-// Below is to workaround unused imports.
+// Below is to workaround unused imports if that happens.
+var _ = BindingContext{}
+var _ = http.MethodGet
 var _ = time.Time{}
 
 
