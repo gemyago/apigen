28efad9d0a5832ceba2890bedef89fa4549a6963
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-02-03 08:58:35 +0100

    Regenerate examples.

--- examples/go-apigen-server/pkg/api/http/v1routes/handlers/pets_params.go
+++ examples/go-apigen-server/pkg/api/http/v1routes/handlers/pets_params.go
@@ -10,7 +10,9 @@ import (
 	. "github.com/gemyago/apigen/examples/go-apigen-server/pkg/api/http/v1routes/internal"
 )
 
-// Below is to workaround unused imports.
+// Below is to workaround unused imports if that happens.
+var _ = BindingContext{}
+var _ = http.MethodGet
 var _ = time.Time{}
 type _ func() Error
 
