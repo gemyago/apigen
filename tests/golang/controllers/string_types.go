8bbc102c0ae9b54b4fcec9ab11d00928d7168543
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-02-05 10:30:18 +0100

    Implement v2 string types controller.

--- tests/golang/controllers/string_types.go
+++ tests/golang/controllers/string_types.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gemyago/apigen/tests/golang/routes/handlers"
 )
 
@@ -33,3 +35,73 @@ func newStringTypesController(
 		HandleStringTypesEnums.With(testActions.stringTypesEnums.action).
 		Finalize()
 }
+
+type stringTypesController struct {
+	testActions *stringTypesControllerTestActions
+}
+
+func (c *stringTypesController) StringTypesArrayItemsRangeValidation(
+	builder *handlers.StringTypesControllerBuilderV2,
+) http.Handler {
+	return builder.StringTypesArrayItemsRangeValidation.HandleWith(
+		c.testActions.stringTypesArrayItemsRangeValidation.action,
+	)
+}
+
+func (c *stringTypesController) StringTypesArraysParsing(
+	builder *handlers.StringTypesControllerBuilderV2,
+) http.Handler {
+	return builder.StringTypesArraysParsing.HandleWith(c.testActions.stringTypesArraysParsing.action)
+}
+
+func (c *stringTypesController) StringTypesEnums(
+	builder *handlers.StringTypesControllerBuilderV2,
+) http.Handler {
+	return builder.StringTypesEnums.HandleWith(c.testActions.stringTypesEnums.action)
+}
+
+func (c *stringTypesController) StringTypesNullableArrayItems(
+	builder *handlers.StringTypesControllerBuilderV2,
+) http.Handler {
+	return builder.StringTypesNullableArrayItems.HandleWith(c.testActions.stringTypesNullableArrayItems.action)
+}
+
+func (c *stringTypesController) StringTypesNullableParsing(
+	builder *handlers.StringTypesControllerBuilderV2,
+) http.Handler {
+	return builder.StringTypesNullableParsing.HandleWith(c.testActions.stringTypesNullableParsing.action)
+}
+
+func (c *stringTypesController) StringTypesNullableRequiredValidation(
+	builder *handlers.StringTypesControllerBuilderV2,
+) http.Handler {
+	return builder.StringTypesNullableRequiredValidation.HandleWith(
+		c.testActions.stringTypesNullableRequiredValidation.action,
+	)
+}
+
+func (c *stringTypesController) StringTypesParsing(
+	builder *handlers.StringTypesControllerBuilderV2,
+) http.Handler {
+	return builder.StringTypesParsing.HandleWith(c.testActions.stringTypesParsing.action)
+}
+
+func (c *stringTypesController) StringTypesPatternValidation(
+	builder *handlers.StringTypesControllerBuilderV2,
+) http.Handler {
+	return builder.StringTypesPatternValidation.HandleWith(c.testActions.stringTypesPatternValidation.action)
+}
+
+func (c *stringTypesController) StringTypesRangeValidation(
+	builder *handlers.StringTypesControllerBuilderV2,
+) http.Handler {
+	return builder.StringTypesRangeValidation.HandleWith(c.testActions.stringTypesRangeValidation.action)
+}
+
+func (c *stringTypesController) StringTypesRequiredValidation(
+	builder *handlers.StringTypesControllerBuilderV2,
+) http.Handler {
+	return builder.StringTypesRequiredValidation.HandleWith(c.testActions.stringTypesRequiredValidation.action)
+}
+
+var _ handlers.StringTypesControllerV2 = &stringTypesController{}
