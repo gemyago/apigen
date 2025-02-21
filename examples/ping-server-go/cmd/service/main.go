e9ca311c9c415e662de55df374b0fedce2d632c6
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-02-21 10:07:53 +0100

    Main router.

---
+++ examples/ping-server-go/cmd/service/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gemyago/apigen/examples/ping-server-go/internal/api/http/v1routes/handlers"
+	"github.com/gemyago/apigen/examples/ping-server-go/internal/api/http/v1routes/models"
+)
+
+// Start it using command below:
+// go run ./examples/ping-server-go/cmd/service/
+//
+// Send requests with curl:
+// - curl localhost:8080/ping
+// - curl localhost:8080/ping?message=hello
+
+type pingController struct{}
+
+func (c *pingController) Ping(b handlers.HandlerBuilder[
+	*handlers.PingPingRequest,
+	*models.Ping200Response,
+]) http.Handler {
+	return b.HandleWith(
+		func(_ context.Context, req *handlers.PingPingRequest) (*models.Ping200Response, error) {
+			message := req.Message
+			if message == "" {
+				message = "pong"
+			}
+			return &models.Ping200Response{Message: message}, nil
+		},
+	)
+}
+
+// pingController must implement handlers.PingController interface.
+var _ handlers.PingController = (*pingController)(nil)
+
+// Simple adapter to use http.ServeMux with generated routes.
+type httpRouter http.ServeMux
+
+func (*httpRouter) PathValue(r *http.Request, paramName string) string {
+	return r.PathValue(paramName)
+}
+
+func (r *httpRouter) HandleRoute(method, pathPattern string, h http.Handler) {
+	mux := (*http.ServeMux)(r)
+	mux.Handle(method+" "+pathPattern, h)
+}
+
+func main() {
+	port := 8080
+	readHeaderTimeoutSec := 2
+
+	mux := http.NewServeMux()
+	httpApp := handlers.NewHTTPApp((*httpRouter)(mux))
+	handlers.RegisterPingRoutes(&pingController{}, httpApp)
+
+	srv := &http.Server{
+		Addr:              fmt.Sprintf("[::]:%d", port),
+		ReadHeaderTimeout: time.Duration(readHeaderTimeoutSec) * time.Second,
+		Handler:           mux,
+	}
+	log.Println("Starting server on port:", port)
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
