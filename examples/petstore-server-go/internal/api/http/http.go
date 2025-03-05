72eebdb9485573fab7a1304935a7e40845c2dcd7
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-03-05 09:35:06 +0100

    Updating generators.

--- examples/petstore-server-go/internal/api/http/http.go
+++ examples/petstore-server-go/internal/api/http/http.go
@@ -2,7 +2,7 @@ package http
 
 // Below will use server generator that was previously built in this project
 // and referencing pre-built server generator module
-// In real world scenario it can be just: //go:generate go run github.com/gemyago/apigen ./routes.yaml ./routes
+// In real world scenario it can be just: //go:generate go run github.com/gemyago/apigen server ./routes.yaml ./routes
 
-//go:generate -command petstore-apigen go run ../../../../../ ./../../../../petstore.yaml --server-generator-location ../../../../../generators/go-apigen-server/target/server.jar
+//go:generate -command petstore-apigen go run github.com/gemyago/apigen server ./../../../../petstore.yaml --server-generator-location ../../../../../generators/go-apigen-server/target/server.jar
 //go:generate petstore-apigen ./routes
