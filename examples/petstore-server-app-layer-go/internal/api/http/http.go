51f59149a9f5af117d484f85506c20c1bec0226b
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-03-06 08:22:19 +0100

    Generate models and routes separately.

--- examples/petstore-server-app-layer-go/internal/api/http/http.go
+++ examples/petstore-server-app-layer-go/internal/api/http/http.go
@@ -2,4 +2,6 @@ package http
 
 // Below will use server generator that was previously built in this project.
 // In real world scenario it can be just: apigen <path-to-openapi.yaml> <output-dir>
-//go:generate go run ../../../../../ ./../../../../petstore.yaml ./routes --server-generator-location ../../../../../generators/go-apigen-server/target/server.jar
+//go:generate -command petstore-apigen go run github.com/gemyago/apigen server ./../../../../petstore.yaml --server-generator-location ../../../../../generators/go-apigen-server/target/server.jar --verbose
+//go:generate petstore-apigen ./routes --global-property apis --model-package "github.com/gemyago/apigen/examples/petstore-server-app-layer-go/internal/app/models"
+//go:generate petstore-apigen ../../app/models --global-property models
